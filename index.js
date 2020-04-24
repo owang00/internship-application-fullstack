4e79addff8684a115eb584318d3c25c79e9eed81
Oliver Wang <ow361@nyu.edu>
2020-04-23 20:00:18 -0500

    Figured out how to fetch URL information
    Summary describes everything. Doing the random part now

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
-/**
- * Respond with hello worker text
- * @param {Request} request
- */
+
 async function handleRequest(request) {
-  return new Response('HIHIHIHI!', {
-    headers: { 'content-type': 'text/plain' },
-  })
+	const response = await fetch(new URL('https://cfw-takehome.developers.workers.dev/api/variants'));
+	let data = await response.json();
+	var urls = data["variants"];
+
+	if(getRandomInt() == 0) {
+		
+	}
+
+
+	/*
+	var1 = data["variants"][0];
+	var2 = data["variants"][1];
+	console.log(var1);
+	console.log(var2);
+	*/ 
+	return new Response(var1+"\n"+var2, {
+		headers: { 'content-type': 'text/plain' },
+	});
+}
+
+function getRandomInt() {
+  return Math.floor(Math.random()*(2));
 }
